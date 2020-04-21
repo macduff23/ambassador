478433e447e1bef3052c71b046161eb6bdab6abd
Shahriar Rostami <srdsecond@gmail.com>
2020-04-21 18:36:17 -0400

    Change the envoy image to use v1.13 with a proper legacy RLS.

--- cxx/envoy.mk
+++ cxx/envoy.mk
@@ -10,7 +10,7 @@ IS_PRIVATE ?= $(findstring private,$(_git_remote_urls))
 
 # IF YOU MESS WITH ANY OF THESE VALUES, YOU MUST RUN `make update-base`.
   ENVOY_REPO ?= $(if $(IS_PRIVATE),git@github.com:datawire/envoy-private.git,git://github.com/datawire/envoy.git)
-  ENVOY_COMMIT ?= a54d6eb82e8e83d65e52307e89557a07c0b6d1b0
+  ENVOY_COMMIT ?= 33ff697fc665b25463c7d64916ee6736782e2e76
   ENVOY_COMPILATION_MODE ?= opt
   # Increment BASE_ENVOY_RELVER on changes to `docker/base-envoy/Dockerfile`, or Envoy recipes.
   # You may reset BASE_ENVOY_RELVER when adjusting ENVOY_COMMIT.
